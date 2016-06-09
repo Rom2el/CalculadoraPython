d5cddab904f93d6dd79af9dc2a59c7a68dfcfd48
KarlaSteff <karliz_steffania@hotmail.com>
2016-06-08 23:21:34 -0500

    Código nuevo
    Nuevo intento

--- Calculadora.py
+++ Calculadora.py
@@ -1,35 +1,66 @@
-from tkinter import *
- 
+from tkinter import * 
 import sys
 
- 
+def limpiar():
+	txtDisplay.delete(0,END)
+	return
+def llenar():
+	
+	return
 
- 
 app = Tk()
+frame= Frame(app)
+frame.pack()
+
 app.title("Calculadora")
 
+num1= StringVar()
 
-	
-#Ventana Principal
-vp = Frame(app)
-vp.grid(column=0, row=0, padx=(50,50), pady=(50,50))
-vp.columnconfigure(1, weight=2)
-vp.rowconfigure(1, weight=2)
+txtDisplay = Entry(frame,textvariable = num1, bd=20,insertwidth=1, font =30)
+txtDisplay.pack(side=TOP)
 
-valor = ""
-entrada_texto = Entry(vp, width=10, textvariable=valor)
-entrada_texto.grid(column=7, row=1)
+frame0 = Frame (app)
+frame0.pack(side = TOP)
+boton1= Button(frame0,padx=16,pady=16,bd=8,text=1,fg="black",command=llenar)
+boton1.pack(side=LEFT)
+boton2= Button(frame0,padx=16,pady=16,bd=8,text=2,fg="black")
+boton2.pack(side=LEFT)
+boton3= Button(frame0,padx=16,pady=16,bd=8,text=3,fg="black")
+boton3.pack(side=LEFT)
+boton4= Button(frame0,padx=16,pady=16,bd=8,text="+",fg="black")
+boton4.pack(side=LEFT)
 
-def botones():
-	num =0
-	for i in [1,3,5]:
-		for j in [1,3,5]:
-			num=num+1
-			boton = Button(vp, text=num, command=hacer_click)
-			boton.grid(column=j, row=i)
+frame1=Frame(app)
+frame1.pack(side=TOP)
+boton5= Button(frame1,padx=16,pady=16,bd=8,text=4,fg="black")
+boton5.pack(side=LEFT)
+boton6= Button(frame1,padx=16,pady=16,bd=8,text=5,fg="black")
+boton6.pack(side=LEFT)
+boton7= Button(frame1,padx=16,pady=16,bd=8,text=6,fg="black")
+boton7.pack(side=LEFT)
+boton8= Button(frame1,padx=16,pady=16,bd=8,text="-",fg="black")
+boton8.pack(side=LEFT)
 
+frame2 = Frame (app)
+frame2.pack(side = TOP)
+boton1= Button(frame2,padx=16,pady=16,bd=8,text=7,fg="black")
+boton1.pack(side=LEFT)
+boton2= Button(frame2,padx=16,pady=16,bd=8,text=8,fg="black")
+boton2.pack(side=LEFT)
+boton3= Button(frame2,padx=16,pady=16,bd=8,text=9,fg="black")
+boton3.pack(side=LEFT)
+boton4= Button(frame2,padx=16,pady=16,bd=8,text="/",fg="black")
+boton4.pack(side=LEFT)
 
-
-botones()
+frame3=Frame(app)
+frame3.pack(side=TOP)
+boton1= Button(frame3,padx=16,pady=16,bd=8,text="C",fg="black",command = limpiar)
+boton1.pack(side=LEFT)
+boton2= Button(frame3,padx=16,pady=16,bd=8,text=0,fg="black")
+boton2.pack(side=LEFT)
+boton3= Button(frame3,padx=16,pady=16,bd=8,text="=",fg="black")
+boton3.pack(side=LEFT)
+boton4= Button(frame3,padx=16,pady=16,bd=8,text="*",fg="black")
+boton4.pack(side=LEFT)
 
 app.mainloop()
